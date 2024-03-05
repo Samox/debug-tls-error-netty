e47427d49a419078be9ce817b4900d4827f38cd1
Sammy Teillet <sammy.teillet@gmail.com>
2024-03-05 14:19:53 +0100

    make a failing ssl handshake

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -20,13 +20,22 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import java.net.InetSocketAddress;
+
 import io.netty.util.CharsetUtil;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLException;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException, URISyntaxException {
+    public static void main(String[] args) throws InterruptedException, URISyntaxException, SSLException {
         System.out.println("Java version: " + System.getProperty("java.version"));
 
         URI uri = new URI("https://api.kolet.com");
@@ -73,6 +82,42 @@ public class Main {
             future.channel().closeFuture().sync();
         } finally {
             workerGroup.shutdownGracefully();
+        }
+
+        // Try SSL handshake
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            SslContext sslContext = SslContextBuilder.forClient()
+                                    .trustManager(InsecureTrustManagerFactory.INSTANCE) // Use an insecure trust manager
+                                                                                        // for testing
+                                    .build();
+                            SSLEngine engine = sslContext.newEngine(ch.alloc());
+                            engine.setUseClientMode(true);
+
+                            // Misconfigure SSL/TLS settings here to intentionally cause handshake failure
+                            // For example:
+                            // engine.setEnabledProtocols(new String[]{"SSLv3"});
+                            // engine.setEnabledCipherSuites(new
+                            // String[]{"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384"});
+
+                            ch.pipeline().addLast(new SslHandler(engine));
+                        }
+                    });
+
+            // Attempt to connect to the server
+            bootstrap.connect(new InetSocketAddress("api.kolet.com", 443)).sync().channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
         }
 
     }
